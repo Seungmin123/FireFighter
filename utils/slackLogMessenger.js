acdf08fd222f1463dac559d018968899ac0f62bd
Seungmin123 <dltmdals147@gmail.com>
2024-01-10 10:05:17 +0900

    - 테스트 코드 수정

--- utils/slackLogMessenger.js
+++ utils/slackLogMessenger.js
@@ -2,8 +2,6 @@ const axios = require('axios');
 
 const SLACK_DEV_URL = process.env.COW_SLACK_DEV_URL;
 
-console.log(SLACK_DEV_URL)
-
 exports.sendSlackLogMessage = async function (posts) {
     let today = new Date();
     today = today.getFullYear() + '-' + String(today.getMonth() + 1).padStart(2, '0') + '-' + String(today.getDate()).padStart(2, '0');
@@ -11,6 +9,8 @@ exports.sendSlackLogMessage = async function (posts) {
     let message = {
         text: today + ' 로그 모니터링! \n\n' + posts.map(post => `${post.company}(${post.creator}) - <${post.link}|${post.title}> (Cat : ${post.categories})`).join('\n') + '\n\n 당분간 비교해봅시다'
     }
+
+    console.log(SLACK_DEV_URL)
 
     axios.post(SLACK_DEV_URL, JSON.stringify(message))
         .then(console.log('Slack Log MSG FINISH'))
